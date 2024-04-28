f9746abf5cc0dca8450ccaac6b0161c1789d946e
Will H <will.wkhu@gmail.com>
2024-04-28 13:17:44 -0700

    Clean file and implement account detail path

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -1,51 +1,35 @@
 const Account = require('../models/account');
-const CommunityInfo = require('../models/community-info');
 const Home = require('../models/home');
-const InteriorInfo = require('../models/interior-info');
-const PropertyInfo = require('../models/property-info');
 const User = require('../models/user');
 
 const asyncHandler = require('express-async-handler');
 
+const accountPath = './pages/admin/account';
+
 // Main page for Admin route
 exports.index = asyncHandler(async (req, res, next) => {
-  const [
-    numAccounts,
-    numCommunityInfos,
-    numHomes,
-    numInteriorInfos,
-    numPropertyInfos,
-    numUsers,
-  ] = await Promise.all([
+  const [numAccounts, numHomes, numUsers] = await Promise.all([
     Account.countDocuments({}).exec(),
-    CommunityInfo.countDocuments({}).exec(),
     Home.countDocuments({}).exec(),
-    InteriorInfo.countDocuments({}).exec(),
-    PropertyInfo.countDocuments({}).exec(),
     User.countDocuments({}).exec(),
   ]);
 
   res.render('./pages/admin/index', {
     title: 'Admin Dashboard',
     accounts_count: numAccounts,
-    communityinfos_count: numCommunityInfos,
     homes_count: numHomes,
-    interiorinfos_count: numInteriorInfos,
-    propertyinfos_count: numPropertyInfos,
     users_count: numUsers,
   });
 });
 
 // Display list of Accounts
 exports.account_list = asyncHandler(async (req, res, next) => {
-  // res.send('NOT IMPLEMENTED: Account List');
-
   const allAccounts = await Account.find({}, 'type user created_on')
     .sort({ type: 1 })
     .populate('user')
     .exec();
 
-  res.render('./pages/admin/account/account-list', {
+  res.render(`${accountPath}/account-list`, {
     title: 'All Accounts',
     accounts_list: allAccounts,
   });
@@ -53,7 +37,22 @@ exports.account_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Account
 exports.account_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Account detail: ${req.params.id}`);
+  const account = await Account.findById(req.params.id)
+    .populate('user')
+    .populate('homes')
+    .exec();
+
+  if (account === null) {
+    // No results
+    const err = new Error('Genre not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render(`${accountPath}/account-detail`, {
+    title: 'Account Details',
+    account: account,
+  });
 });
 
 // Display Account create form on GET
